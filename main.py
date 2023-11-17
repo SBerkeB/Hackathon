e728649bbf71b3251e404427295d7fc15365e636
Sami Berke Babaoglu <comradekedi@Samis-MacBook-Air.local>
2023-11-17 23:20:44 +0300

    html folder push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 # from fastapi.staticfiles import StaticFiles
-# from fastapi.templating import Jinja2Templates
+from fastapi.templating import Jinja2Templates
 from pymongo import MongoClient
 from dotenv import dotenv_values
 
@@ -14,9 +14,11 @@ CFG = dotenv_values(".env")
 
 
 app = FastAPI()
+
 app.include_router(database.router)
 app.include_router(user_access.router)
 
+templates = Jinja2Templates(directory="/html")
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # templates = Jinja2Templates(directory="templates")
