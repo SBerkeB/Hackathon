e1617e1c2c2fdeef0c68c0a895fc944bf5cd62e4
Burgazl <149014559+Burgazl@users.noreply.github.com>
2023-11-18 17:55:03 +0300

    Update swirl.sol

--- swirl.sol
+++ swirl.sol
@@ -1,9 +1,7 @@
-// File: @openzeppelin/contracts/utils/Context.sol
 // SPDX-License-Identifier: MIT
 
-
+// File: @openzeppelin/contracts/utils/Context.sol
 // OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
-
 pragma solidity ^0.8.0;
 
 abstract contract Context {
@@ -17,39 +15,30 @@ abstract contract Context {
 }
 
 // File: @openzeppelin/contracts/access/Ownable.sol
-
-
 // OpenZeppelin Contracts (last updated v4.9.0) (access/Ownable.sol)
-
 pragma solidity ^0.8.0;
-
 
 abstract contract Ownable is Context {
     address private _owner;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    
     constructor() {
         _transferOwnership(_msgSender());
     }
 
-    
     modifier onlyOwner() {
         _checkOwner();
         _;
     }
 
-   
     function owner() public view virtual returns (address) {
         return _owner;
     }
 
-   
     function _checkOwner() internal view virtual {
         require(owner() == _msgSender(), "Ownable: caller is not the owner");
     }
-
 
     function renounceOwnership() public virtual onlyOwner {
         _transferOwnership(address(0));
@@ -66,14 +55,12 @@ abstract contract Ownable is Context {
         emit OwnershipTransferred(oldOwner, newOwner);
     }
 }
+
 pragma solidity ^0.8.9;
-
-
 
 contract Swirl is Ownable {
     address public protocolFeeDestination;
     uint256 public protocolFeePercent;
-
 
     event Trade(
         address trader,
@@ -85,14 +72,28 @@ contract Swirl is Ownable {
         uint256 supply
     );
 
+    event BuySharesEvent(
+        address indexed buyer,
+        address indexed sharesSubject,
+        uint256 amount,
+        uint256 price
+    );
+
+    event SellSharesEvent(
+    address indexed seller,
+    address indexed sharesSubject,
+    uint256 amount,
+    uint256 price
+);
+
+
     // SharesSubject => (Holder => Balance)
     mapping(address => mapping(address => uint256)) public sharesBalance;
 
     // SharesSubject => Supply
     mapping(address => uint256) public sharesSupply;
 
-    constructor() Ownable(){
-
+    constructor() Ownable() {
         protocolFeePercent = 70000000000000000;
     }
 
@@ -100,15 +101,12 @@ contract Swirl is Ownable {
         protocolFeeDestination = _feeDestination;
     }
 
-
     function setProtocolFeePercent(uint256 _feePercent) public onlyOwner {
         protocolFeePercent = _feePercent;
     }
 
-
-
     function getPrice(uint256 supply, uint256 amount) public pure returns (uint256) {
-        uint256 sum1 = supply == 0 ? 0 : (supply - 1 )* (supply) * (2 * (supply - 1) + 1) / 6;
+        uint256 sum1 = supply == 0 ? 0 : (supply - 1) * (supply) * (2 * (supply - 1) + 1) / 6;
         uint256 sum2 = supply == 0 && amount == 1 ? 0 : (supply - 1 + amount) * (supply + amount) * (2 * (supply - 1 + amount) + 1) / 6;
         uint256 summation = sum2 - sum1;
         return summation * 1 ether / 16000;
@@ -125,13 +123,13 @@ contract Swirl is Ownable {
     function getBuyPriceAfterFee(address sharesSubject, uint256 amount) public view returns (uint256) {
         uint256 price = getBuyPrice(sharesSubject, amount);
         uint256 protocolFee = price * protocolFeePercent / 1 ether;
-        return price + protocolFee ;
+        return price + protocolFee;
     }
 
     function getSellPriceAfterFee(address sharesSubject, uint256 amount) public view returns (uint256) {
         uint256 price = getSellPrice(sharesSubject, amount);
         uint256 protocolFee = price * protocolFeePercent / 1 ether;
-        return price - protocolFee ;
+        return price - protocolFee;
     }
 
     function buyShares(address sharesSubject, uint256 amount) public payable {
@@ -139,8 +137,8 @@ contract Swirl is Ownable {
         require(supply > 0 || sharesSubject == msg.sender, "First share should be bought by account owner.");
         uint256 price = getPrice(supply, amount);
         uint256 protocolFee = price * protocolFeePercent / 1 ether;
-        require(msg.value >= price + protocolFee , "Not enough balance.");
-        sharesBalance[sharesSubject][msg.sender] = sharesBalance[sharesSubject][msg.sender] + amount;
+        require(msg.value >= price + protocolFee, "Not enough balance.");
+        sharesBalance[sharesSubject][msg.sender] += amount;
         sharesSupply[sharesSubject] = supply + amount;
         emit Trade(
             msg.sender,
@@ -151,30 +149,49 @@ contract Swirl is Ownable {
             protocolFee,
             supply + amount
         );
+        emit BuySharesEvent(
+            msg.sender,
+            sharesSubject,
+            amount,
+            price
+        );
         (bool success1, ) = protocolFeeDestination.call{value: protocolFee}("");
-        require(success1 , "Unable to send funds");
+        require(success1, "Unable to send protocol fee");
     }
 
     function sellShares(address sharesSubject, uint256 amount) public payable {
-        uint256 supply = sharesSupply[sharesSubject];
-        require(supply > amount, "Last share cannot be sell");
-        uint256 price = getPrice(supply - amount, amount);
-        uint256 protocolFee = price * protocolFeePercent / 1 ether;
-        require(sharesBalance[sharesSubject][msg.sender] >= amount, "Not enought money.");
-        sharesBalance[sharesSubject][msg.sender] = sharesBalance[sharesSubject][msg.sender] - amount;
-        sharesSupply[sharesSubject] = supply - amount;
-        emit Trade(
-            msg.sender,
-            sharesSubject,
-            false,
-            amount,
-            price,
-            protocolFee,
-            supply - amount
-        );
-        (bool success1, ) = msg.sender.call{value: price - protocolFee }("");
-        (bool success2, ) = protocolFeeDestination.call{value: protocolFee }("");
+    uint256 supply = sharesSupply[sharesSubject];
+    require(supply > amount, "Last share cannot be sold");
+    uint256 price = getPrice(supply - amount, amount);
+    uint256 protocolFee = price * protocolFeePercent / 1 ether;
+    require(sharesBalance[sharesSubject][msg.sender] >= amount, "Not enough shares.");
 
-        require(success1 && success2 , "Unable to send funds");
-    }
+    sharesBalance[sharesSubject][msg.sender] -= amount;
+    sharesSupply[sharesSubject] = supply - amount;
+
+    // Emit the Trade event with additional details
+    emit Trade(
+        msg.sender,
+        sharesSubject,
+        false,
+        amount,
+        price,
+        protocolFee,
+        supply - amount
+    );
+
+    // Additional logs for the website frontend
+    emit SellSharesEvent(
+        msg.sender,
+        sharesSubject,
+        amount,
+        price
+    );
+
+    // Send funds to the seller and protocol fee to the destination
+    (bool success1, ) = msg.sender.call{value: price - protocolFee}("");
+    (bool success2, ) = protocolFeeDestination.call{value: protocolFee}("");
+    require(success1 && success2, "Unable to send funds");
+}
+
 }
